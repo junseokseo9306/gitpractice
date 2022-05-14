d328b7a2b4f7eb4718e7275c9474e9c53d60b5ff
junseokseo <junseoks9306@gmail.com>
2022-05-14 12:14:53 +0900

    base fragment 생성

---
+++ app/src/main/java/com/example/myapplication/baseFragment.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class baseFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
